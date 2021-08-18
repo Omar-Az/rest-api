7f9910a60278195418bba1a764b2a5c9c8883ef8
Omar-Az <71438315+Omar-Az@users.noreply.github.com>
2021-08-19 01:28:42 +0500

    1st comit

--- rest-api/controllers/userController.js
+++ rest-api/controllers/userController.js
@@ -1,23 +1,25 @@
-const express = require('express')
-const router = express.Router()
-let users = [
+const express=require("express")
+const router=express.Router()
+let users=[
     {
-        userName: "Arslan",
-        userid: 1
+    userName:"Omar",
+    userID:1
     },
     {
-        userName: "Ali",
-        userid: 2
-    }
+        userName:"Aziz",
+        userID:2
+        }
 ]
-router.post('/addUser', (req, res) => {
-    console.log(req.body)
-    users.push(req.body)
-    res.status(201).send(req.body)
-})
-router.get('/viewUser', (req, res) => {
-
+ //post
+ router.post('/addUser',(req,res)=>{
+     console.log(req.body)
+     users.push(req.body)  
+     res.status(201).send(req.body)
+ })
+//request
+router.get('/viewUser',(req,res) => {
+  
     res.status(200).json(users)
 })
 
-module.exports = router
+module.exports= router
--- rest-api/server.js
+++ rest-api/server.js
@@ -1,24 +1,26 @@
-const express = require("express")
-const bodyparser = require("body-parser")
-const cors= require("cors")
+const express = require('express')
+const bodyparser= require("body-parser")
+const cors=require("cors")
 const port=3000
 
 
-// controllers
-const users = require('./controllers/userController')
+//controller
+const users= require('./controllers/userController')
+//the import function mentioned in 
+//the userContorller.js will sent 
+//all the data in the above variable 'users'
 
 
-const app = express()
 
+const app=express()
 app.use(cors())
 
 app.use(bodyparser.json())
-app.use('/users',users)
+app.use('/user',users)
 app.listen(port,function(){
-    console.log("server started at port "+ port)
+    console.log("Server started at port" + port)
 })
-
-
+    //localhost:3000
 app.get('/',function(req,res){
-    res.send("Your server started at port "+ port)
+    res.send("Server is running")
 })
